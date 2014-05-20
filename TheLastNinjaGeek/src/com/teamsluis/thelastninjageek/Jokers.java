4e80b4247e5b2f02e0078c6596028b4abb3ba79d
Siujade <siujade@gmail.com>
2014-05-20 22:13:23 +0300

    Jokers and questions

--- TheLastNinjaGeek/src/com/teamsluis/thelastninjageek/Jokers.java
+++ TheLastNinjaGeek/src/com/teamsluis/thelastninjageek/Jokers.java
@@ -1,5 +1,6 @@
 package com.teamsluis.thelastninjageek;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,6 +15,8 @@ public class Jokers {
 				jokersWindow[i] = new JButton();
 				jokersWindow[i].setEnabled(false);
 				jokersWindow[i].setVisible(true);
+				jokersWindow[i].setForeground(Color.WHITE);
+				jokersWindow[i].setBackground(Color.BLACK);
 				jokersWindow[i].setBounds(720, 60 + ((i + 1) * 70), 70, 60);
 				;
 				Main.gameWindow.add(jokersWindow[i]);
@@ -31,6 +34,7 @@ public class Jokers {
 
 					}
 				}
+				// Sudden strike joker action performed   = Player points multiplied x 2;
 				jokersWindow[1].addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						Data.scorePlayer[Data.currentPlayer]++;
@@ -43,9 +47,57 @@ public class Jokers {
 										Data.currentPlayerJokers[Data.currentPlayer][1],
 										Data.currentPlayer,
 										Data.playerHasJoker[Data.currentPlayer]);
-
 					}
 				});
+				// 50/50 (Ninja HACK) removes 2 of the answers;
+				
+				jokersWindow[0].addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						int counter = 0;
+						for (int i = 0; i <  Game.buttons.length; i++) {							
+		                 if (Game.buttons[i].getText().equals(Game.CorA)) {
+		                
+							continue;	
+							
+						}
+		                 else if (counter < 2){
+		                	 counter++;
+		                	 Game.buttons[i].setVisible(false);
+		                 }
+					}
+						
+						System.out.println("50/50 used");
+						System.out.println(Game.CorA);
+						
+						jokersWindow[0].setEnabled(false);
+						Data.currentPlayerJokers[Data.currentPlayer][0]--;
+						Data.playerHasJoker[Data.currentPlayer]--;
+						System.out
+								.printf("%nPlayer %d current 50/50 jokers are %d.%nPlayer %d has %d jokers left.",
+										Data.currentPlayer,
+										Data.currentPlayerJokers[Data.currentPlayer][0],
+										Data.currentPlayer,
+										Data.playerHasJoker[Data.currentPlayer]);
+					}
+				});
+				
+	           // Nerds Rage  change the question.				
+				jokersWindow[2].addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {			
+						Game.nextQuestion();		
+						Game.questions--;
+						jokersWindow[2].setEnabled(false);
+						Data.currentPlayerJokers[Data.currentPlayer][2]--;
+						Data.playerHasJoker[Data.currentPlayer]--;
+						System.out
+								.printf("%nPlayer %d current NERD RAGE  jokers are %d.%nPlayer %d has %d jokers left.",
+										Data.currentPlayer,
+										Data.currentPlayerJokers[Data.currentPlayer][2],
+										Data.currentPlayer,
+										Data.playerHasJoker[Data.currentPlayer]);
+					}
+				});
+				
 			}
 		}
 	}
